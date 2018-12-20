1aa4dcff466df61a21d4b762a0a0e12aa94747e5
Cherie Vasta <cherie@dragonfly.co.nz>
2018-12-20 15:30:03 +1300

    Initial commit

---
+++ secret_README.py
@@ -0,0 +1,5 @@
+# To use this code, go to KoreroMaori.io, login, get the tokens from the bottom of the xxx page
+# put the actual tokens below then rename this file to secret.py
+token = "xxxxx"
+token_nondev = 'xxx'
+
