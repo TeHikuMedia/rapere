6ca3b032b4095b412453a03e889b0ed81dc30efc
Edward Abraham <edward@dragonfly.co.nz>
2019-02-01 09:02:56 +1300

    Update secret_README.py

--- secret_README.py
+++ secret_README.py
@@ -1,4 +1,4 @@
-# To use this code, go to KoreroMaori.io, login, and get yuor token from the bottom of https://koreromaori.io/dashboard/
+# To use this code, go to KoreroMaori.io, login, and get your token from the bottom of https://koreromaori.io/dashboard/
 # Put your actual token below in place of xxxx then rename this file to secret.py
 
 token = 'xxxx'
